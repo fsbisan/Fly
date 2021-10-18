c564421047ce39d0fc759e391da8d86bd7f7232c
Alexander <fsbisan@mail.ru>
2021-10-18 21:34:32 +0500

    Initial Commit

---
+++ Fly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fly
+//
+//  Created by Александр Макаров on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
