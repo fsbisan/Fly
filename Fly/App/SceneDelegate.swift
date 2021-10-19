8c4f6c4be8e9a11b7f06e5a7f66b86c0838460f4
Alexander <fsbisan@mail.ru>
2021-10-19 22:24:07 +0500

    Set board view controller

--- Fly/App/SceneDelegate.swift
+++ Fly/App/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = StartViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Fly/ViewControllers/BoardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BoardViewController.swift
+//  Fly
+//
+//  Created by Александр Макаров on 19.10.2021.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
