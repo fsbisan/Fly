cf36ea441d1d2a5041f7f5e6a881977e6b2351a2
Alexander <fsbisan@mail.ru>
2021-10-19 22:31:49 +0500

    Black view controller

--- Fly/ViewControllers/BoardViewController.swift
+++ Fly/ViewControllers/BoardViewController.swift
@@ -11,6 +11,6 @@ class BoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .black
     }
 }
